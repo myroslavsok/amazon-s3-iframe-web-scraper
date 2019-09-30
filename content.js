3f732f97f864a59d3885f3aa09702fcfc5d0fb6c
Myroslav Sokolov <myroslavso@softwareplanet.uk.com>
2019-09-30 12:16:48 +0300

    - add ability to find iframe automatically - add ability to go to iframe scr - allow user to extract exhibitors links automatically

--- content.js
+++ content.js
@@ -1,12 +1,54 @@
 console.log("content works");
 
+// document.getElementById('webModuleHolderIframe').onload= function() {
+//     console.log('loaded', document.getElementById('webModuleHolderIframe'));
+// };
+
 const ACTIONS = {
-    GET_CONTENT_PAGE_URL: 'get-content-page-url'
+    GET_CURRENT_PAGE_URL: 'get-content-page-url',
+    GET_TARGET_A_HREFS: 'get-target-a-hrefs',
+    GET_IFRAME_PRODUCT_LINKS: 'get-iframe-product-links',
+
+    FIND_IFRAME_ACTION: 'find-iframe-action',
+    GET_TARGET_LINKS: 'get-target-links',
 };
 
+// Message listener
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === ACTIONS.GET_CONTENT_PAGE_URL) {
-        sendResponse({contentPageUrl: window.location.href});
+    switch (request.action) {
+        // case ACTIONS.GET_CURRENT_PAGE_URL:
+        //     sendResponse(window.location.href); // Get current page URL
+        //     break;
+        case ACTIONS.FIND_IFRAME_ACTION:
+            let response = {
+                iframeSrc: getIframeSrc(),
+                currentPageUrl: window.location.href
+            };
+            sendResponse(response);
+            break;
+        case ACTIONS.GET_TARGET_LINKS:
+            sendResponse(getTargetAHrefsBySelector(request.currentPageUrl));
+            break;
     }
 });
+
+// Get iframe src so as to go to new tab
+function getIframeSrc() {
+    const iframe = document.body.querySelector('iframe');
+    iframe.style.border = '2px solid blue';
+    console.log('iframe', iframe);
+    console.log('iframe', iframe.src);
+    return iframe.src;
+}
+
+// Get links for fetching
+function getTargetAHrefsBySelector(currentPageUrl) {
+    const selector = `a[href^="${currentPageUrl}"]`; // Selector for extracting products' links
+    let targetAHrefsArr = [];
+    document.body.querySelectorAll(selector).forEach(targetElement => {
+        targetAHrefsArr.push(targetElement.getAttribute('href'));
+    });
+    console.log('links', targetAHrefsArr);
+    return targetAHrefsArr;
+}
 
--- popup.js
+++ popup.js
@@ -1,38 +1,95 @@
 console.log('popup works');
 
 const ACTIONS = {
-    GET_CONTENT_PAGE_URL: 'get-content-page-url'
-};
+    GET_CURRENT_PAGE_URL: 'get-content-page-url',
+    GET_TARGET_A_HREFS: 'get-target-a-hrefs',
+    GET_IFRAME_PRODUCT_LINKS: 'get-iframe-product-links',
 
-let currentContentPageUrl = '';
+    FIND_IFRAME_ACTION: 'find-iframe-action',
+    GET_TARGET_LINKS: 'get-target-links',
+};
 
 // Popup onOpen
-window.onload = function() {
-    chrome.tabs.query({active: true, currentWindow: true}, setCurrentContentPageUrl)
+window.onload = function () {
+    // const newURL = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
+    // chrome.tabs.create({ url: newURL });
+
+    // chrome.tabs.query({active: true, currentWindow: true}, setCurrentContentPageUrl)
+
+    // chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    //     const message = {
+    //         action: ACTIONS.GET_CURRENT_PAGE_URL
+    //     };
+    //     chrome.tabs.sendMessage(tabs[0].id, message, response => {
+    //         currentContentPageUrl = response;
+    //         console.log('onload', currentContentPageUrl);
+    //         setCurrentContentPageUrl(currentContentPageUrl);
+    //     });
+    // });
 };
-function setCurrentContentPageUrl(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {action: ACTIONS.GET_CONTENT_PAGE_URL}, function(response) {
-        console.log('response', response.contentPageUrl);
-        currentContentPageUrl = response.contentPageUrl;
-        document.getElementById('currentUrl').textContent += currentContentPageUrl;
+
+const findIframeBtn = document.getElementById('findIframeBtn');
+findIframeBtn.addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        const message = {
+            action: ACTIONS.FIND_IFRAME_ACTION
+        };
+        chrome.tabs.sendMessage(tabs[0].id, message, pageAndIframeUrls => {
+            console.log('findIframeBtn resp', pageAndIframeUrls);
+            // pageAndIframeUrls includes .iframeSrc && .currentPageUrl
+            chrome.storage.sync.set({ 'pageAndIframeUrls': pageAndIframeUrls });
+            // TODO Mirek add null handler
+            if (window.confirm('Would you like to go to iframe link?')) {
+                chrome.tabs.create({ url: pageAndIframeUrls.iframeSrc });
+            }
+        });
     });
-}
+}, true);
+
+const getTargetLinksBtn = document.getElementById('getTargetLinksBtn');
+getTargetLinksBtn.addEventListener('click', () => {
+    let currentPageUrl = '';
+    chrome.storage.sync.get(['pageAndIframeUrls'], result => {
+        currentPageUrl = result.pageAndIframeUrls.currentPageUrl;
+    });
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        const message = {
+            action: ACTIONS.GET_TARGET_LINKS,
+            currentPageUrl: currentPageUrl
+        };
+        console.log('message', message);
+        chrome.tabs.sendMessage(tabs[0].id, message, response => {
+            console.log('getTargetLinksBtn resp', response);
+        });
+    });
+}, true);
+
+
+// function setCurrentContentPageUrl(currentPageUrl) {
+//     document.getElementById('currentUrl').textContent += currentPageUrl;
+// }
 
 // Run scraper
-const scrapeBtn = document.getElementById('scrapeBtn');
-scrapeBtn.addEventListener('click', runScraper, true);
-function runScraper(e) {
-    e.preventDefault();
-    console.log('test');
-    const iframeLink = document.getElementById('iframeLink').value;
-    const aSelector = document.getElementById('aSelector').value;
-    console.log('iframeLink', iframeLink);
-    console.log('aSelector', aSelector);
-}
+// const scrapeForm = document.getElementById('scrapeForm');
+// scrapeForm.addEventListener('submit', runScraper, true);
+// function runScraper(e) {
+//     e.preventDefault();
+//     const iframeLink = document.getElementById('iframeLink').value;
+//     // const aSelector = document.getElementById('aSelector').value;
+//     console.log('iframeLink', iframeLink);
+//     console.log('aSelector', aSelector);
+//     getTargetAHrefs(aSelector);
+// }
 
-
-
-
+// Get links for fetching
+// function getTargetAHrefs(selector) {
+//     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+//         const message = {action: ACTIONS.GET_TARGET_A_HREFS, selector: selector};
+//         chrome.tabs.sendMessage(tabs[0].id, message, response => {
+//             console.log('linkArray', response);
+//         });
+//     })
+// }
 
 
 // function startScraping() {
@@ -54,28 +111,28 @@ function runScraper(e) {
 //   });
 // };
 
-document.addEventListener('DOMContentLoaded', function() {
-  // const scrapeBtn = document.getElementById('scrapeBtn');
-
-  // scrapeBtn.
-  //
-  // var checkPageButton = document.getElementById('checkPage');
-  //
-  // checkPageButton.addEventListener('click', function() {
-  //
-  //   chrome.tabs.getSelected(null, function(tab) {
-  //     d = document;
-  //
-  //     var f = d.createElement('form');
-  //     f.action = 'http://gtmetrix.com/analyze.html?bm';
-  //     f.method = 'post';
-  //     var i = d.createElement('input');
-  //     i.type = 'hidden';
-  //     i.name = 'url';
-  //     i.value = tab.url;
-  //     f.appendChild(i);
-  //     d.body.appendChild(f);
-  //     f.submit();
-  //   });
-  // }, false);
-}, false);
+// document.addEventListener('DOMContentLoaded', function () {
+//     // const scrapeBtn = document.getElementById('scrapeBtn');
+//
+//     // scrapeBtn.
+//     //
+//     // var checkPageButton = document.getElementById('checkPage');
+//     //
+//     // checkPageButton.addEventListener('click', function() {
+//     //
+//     //   chrome.tabs.getSelected(null, function(tab) {
+//     //     d = document;
+//     //
+//     //     var f = d.createElement('form');
+//     //     f.action = 'http://gtmetrix.com/analyze.html?bm';
+//     //     f.method = 'post';
+//     //     var i = d.createElement('input');
+//     //     i.type = 'hidden';
+//     //     i.name = 'url';
+//     //     i.value = tab.url;
+//     //     f.appendChild(i);
+//     //     d.body.appendChild(f);
+//     //     f.submit();
+//     //   });
+//     // }, false);
+// }, false);
